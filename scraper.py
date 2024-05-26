9db4e235e7aa43e37e42c63d3da5e6038cd08d95
FirasKahlaoui <kahlaouifiras2017@gmail.com>
2024-05-27 00:43:12 +0100

    Fix the HTML Tags

--- scraper.py
+++ scraper.py
@@ -2,39 +2,47 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+
 def fetch_bbc_headlines():
     url = 'https://www.bbc.com/news'
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    headlines = [h3.text for h3 in soup.find_all('h3')]
+    headlines = [h2.text for h2 in soup.find_all('h2')]
     return headlines
+
 
 def fetch_cnn_headlines():
     url = 'https://www.cnn.com/world'
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    headlines = [span.text for span in soup.find_all('span', class_='cd__headline-text')]
+    headlines = [span.text for span in soup.find_all(
+        'span', class_='container__headline-text')]
     return headlines
 
-def fetch_reuters_headlines():
-    url = 'https://www.reuters.com/news/world'
+
+"""def fetch_reuters_headlines():
+    url = 'https://www.reuters.com/world/'
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    headlines = [h2.text for h2 in soup.find_all('h2', class_='story-title')]
+    headlines = [a.text for a in soup.find_all(
+        'h3', class_='text__text__1FZLe text__dark-grey__3Ml43 text__medium__1kbOh text__heading_6__1qUJ5 heading__base__2T28j heading__heading_6__RtD9P')]
     return headlines
+"""
+
 
 def save_headlines_to_csv(data, filename):
     df = pd.DataFrame(data, columns=['Source', 'Headline'])
     df.to_csv(filename, index=False)
 
+
 if __name__ == "__main__":
     bbc_headlines = fetch_bbc_headlines()
     cnn_headlines = fetch_cnn_headlines()
-    reuters_headlines = fetch_reuters_headlines()
+    # reuters_headlines = fetch_reuters_headlines()
 
     all_headlines = []
     all_headlines.extend([('BBC', headline) for headline in bbc_headlines])
     all_headlines.extend([('CNN', headline) for headline in cnn_headlines])
-    all_headlines.extend([('Reuters', headline) for headline in reuters_headlines])
+    # all_headlines.extend([('Reuters', headline) for headline in reuters_headlines])
 
     save_headlines_to_csv(all_headlines, 'headlines.csv')
